af598b05e441a0127bd354c22783720b20247d7b
covertseth <40729060+covertseth@users.noreply.github.com>
2019-05-10 20:40:08 -0500

    Add files via upload

---
+++ Pets/Pets/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Pets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+               public class Pet
+        {
+            public string type;
+            public string name;
+            public string owner;
+            public double weight;
+
+            Pet pet1 = new Pet("dog", "Shadow", "Jose", 42.6);
+
+            //This will fill inform the user on the pets name and weight
+            Console.WriteLine("Name: " + pet1.name);
+            Console.WriteLine("Weight: " + pet1.weight);
+            Console.WriteLine(pet1.getTag());
+
+            Console.WriteLine("");
+
+            Dog dog1 = new Dog("Daisy", "Kent", 23.4);
+            //Displays dog 1 info
+            Console.WriteLine("Name: " + dog1.name);
+            Console.WriteLine("Weight: " + dog1.weight);
+            Console.WriteLine(dog1.getTag());
+            Console.WriteLine(dog1.bark(4));
+
+            Console.WriteLine("");
+
+            Cat cat1 = new Cat("Simba", "Maria", 5.2);
+            //Displays cats info
+            Console.WriteLine("Name: " + cat1.name);
+            Console.WriteLine("Weight: " + cat1.weight);
+            Console.WriteLine(cat1.getTag());
+            Console.WriteLine(cat1.meow(3));
+        }
+    }
+}
